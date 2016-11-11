0486b895806824397b562498619b0050cdf48807
Romain Maneschi <romain@maneschi.fr>
2016-11-11 22:21:12 +0100

    add a link to access test with token

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -8,13 +8,23 @@ export default class MainPage {
 
     start(pageContainer) {
         const mainPage = elementBuilder('div', 'mainPage', null, pageContainer).build();
+        const linkTests = elementBuilder('div', 'mainPage-linkTests', {innerHTML: 'Run tests >', onclick: () => {
+            document.querySelector('.mainPage-linkTests-token').style.display = 'inline-block';
+        }}, pageContainer).build();
+        elementBuilder('div', 'mainPage-linkTests-token', null, linkTests)
+            .appendChild('input', 'mainPage-linkTests-token-input', {type: 'text', placeholder: 'token', value: localStorage.getItem('tokenTests')})
+            .appendChild('button', 'mainPage-linkTests-token-btn', {innerHTML: '>', onclick: () => {
+                const token = document.querySelector('.mainPage-linkTests-token-input').value;
+                localStorage.setItem('tokenTests', token);
+                window.location.href = `tests/tests.html?token=${token}`;
+            }});
         [1, 2, 3, 4, 5].forEach(i => mainPage.appendChild(MainPage.buildFireworksLink(i)));
     }
 
     static buildFireworksLink(i) {
-        return elementBuilder('a', 'fireworksLink', {href: `#page=fireworks&nb=${i}`})
-            .appendChild('img', 'fireworksLink-img', {src: 'assets/fireworks-icon.png'})
-            .appendChild('span', 'fireworksLink-number', {innerHTML: i})
+        return elementBuilder('a', 'mainPage-fireworksLink', {href: `#page=fireworks&nb=${i}`})
+            .appendChild('img', 'mainPage-fireworksLink-img', {src: 'assets/fireworks-icon.png'})
+            .appendChild('span', 'mainPage-fireworksLink-number', {innerHTML: i})
             .build();
     }
 
