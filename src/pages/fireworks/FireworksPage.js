99158ba62c3239a33a5de892c550eefe13b1826e
Romain Maneschi <romain@maneschi.fr>
2016-11-11 23:52:17 +0100

    clean code

--- src/pages/fireworks/FireworksPage.js
+++ src/pages/fireworks/FireworksPage.js
@@ -17,15 +17,20 @@ export default class FireworksPage {
         }, pageContainer).build();
 
         fetchAll(implementations, pageParams.nb || 1).then(data => {
-            console.log(data);
-            data.sort((a, b) => a.time - b.time);
-            const winners = data.filter(d => d.error === false);
-            const winner = winners[0];
-            const errors = data.filter(d => d.error !== false);
-            FireworkDisplay.launchText(canvas, !!winner ? winner.winners.map(d => `${d.first_name}.${d.last_name}`).join(' ') : 'FAIL !!');
-            setTimeout(() => pageContainer.appendChild(FireworksPage.buildDetails(winner, winners.filter((a, i) => i>0), errors)), 20000);
+            const {winner, loosers, errors} = FireworksPage.group(data);
+            FireworkDisplay.launchText(canvas, !!winner ? FireworksPage.getStringAttendees(winner, ' ') : 'FAIL !!');
+            setTimeout(() => pageContainer.appendChild(FireworksPage.buildDetails(winner, loosers, errors)), 20000);
         });
 
+    }
+
+    static group(data) {
+        data.sort((a, b) => a.time - b.time);
+        const winners = data.filter(d => d.error === false);
+        const winner = winners[0];
+        const loosers = winners.filter((a, i) => i>0)
+        const errors = data.filter(d => d.error !== false);
+        return {winner, loosers, errors};
     }
 
     static buildDetails(winner, loosers, errors) {
@@ -34,16 +39,16 @@ export default class FireworksPage {
         const oneErrorAtLeast = errors.length > 0;
         return elementBuilder('div', 'fireworks-details')
             .appendChildIf(oneWinner, () => ['div', 'fireworks-details-title', {innerHTML: 'Winner'}])
-            .appendChildIf(oneWinner, () => ['ul', 'fireworks-details-list', {innerHTML: `<li>${winner.config.language} <i>(${winner.time}msc)</i> : ${FireworksPage.getStringAttendees(winner)}</li>`}])
+            .appendChildIf(oneWinner, () => ['ul', 'fireworks-details-list', {innerHTML: `<li>${winner.config.language} <i>(${winner.time}msc)</i> : ${FireworksPage.getStringAttendees(winner, ' // ')}</li>`}])
             .appendChildIf(oneLooserAtLeast, () => ['div', 'fireworks-details-title', {innerHTML: 'Looser'}])
-            .appendChildIf(oneLooserAtLeast, () => ['ul', 'fireworks-details-list', {innerHTML: `${loosers.map(d => `<li>${d.config.language}  <i>(${d.time}msc)</i> : ${FireworksPage.getStringAttendees(d)}</li>`).join('')}`}])
+            .appendChildIf(oneLooserAtLeast, () => ['ul', 'fireworks-details-list', {innerHTML: `${loosers.map(d => `<li>${d.config.language}  <i>(${d.time}msc)</i> : ${FireworksPage.getStringAttendees(d, ' // ')}</li>`).join('')}`}])
             .appendChildIf(oneErrorAtLeast, () => ['div', 'fireworks-details-title', {innerHTML: 'Error'}])
             .appendChildIf(oneErrorAtLeast, () => ['ul', 'fireworks-details-list', {innerHTML: `${errors.map(d => `<li>${d.config.language} <i>(${d.time}msc)</i> : ${d.error.message}</li>`).join('')}`}])
             .build();
     }
 
-    static getStringAttendees(implem) {
-        return implem.winners.map(d => `${d.first_name}.${d.last_name}`).join(' // ');
+    static getStringAttendees(implem, separator) {
+        return implem.winners.map(d => `${d.first_name}.${d.last_name}`).join(separator);
     }
 
 }
